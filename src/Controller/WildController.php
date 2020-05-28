e3430a8239671232f93124df80131d679ee5253f
AminaChakirDev <amina.aitm@gmail.com>
2020-05-28 18:52:02 +0200

    quete 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -15,4 +15,21 @@ Class WildController extends AbstractController
     {
         return $this->render('wild/index.html.twig', ['website' => 'Wild Séries']);
     }
+
+    /**
+     * @Route("/wild/show/{slug<[a-z0-9-]{1,}>}", name="wild_show")
+     */
+    public function show(string $slug) : Response
+    {
+            $slug = ucwords(str_replace('-', ' ', $slug));
+            return $this->render('wild/show.html.twig', ['slug' => $slug]);
+    }
+
+    /**
+     * @Route("/wild/show", name="wild_noshow")
+     */
+    public function noshow() : Response
+    {
+        return $this->render('wild/show.html.twig', ['slug' => 'Aucune série sélectionnée, veuillez choisir une série']);
+    }
 }
